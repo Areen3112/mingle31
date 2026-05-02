b4f00a246f182c3c50ca72f78230f6f92b37a337
WebCrafters <webcrafters7415@gmail.com>
2026-05-02 18:32:34 +0530

    Changes

--- services/emailService.js
+++ services/emailService.js
@@ -1,22 +1,15 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require('resend');
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, text) => {
   try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
+    await resend.emails.send({
+      from: 'Mingle <onboarding@resend.dev>',
       to,
       subject,
       text
     });
-
     console.log("📧 Email sent to:", to);
   } catch (error) {
     console.error("Email error:", error.message);
